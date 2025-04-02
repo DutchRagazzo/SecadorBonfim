1386b9cf4c75c893d71f8fd2576457233fd51a56
Gabriel Holanda <139644915+DutchRagazzo@users.noreply.github.com>
2025-04-01 21:09:53 -0300

    Update codesecadoranalogpatch.ino

--- analogic_version/codesecadoranalogpatch.ino
+++ analogic_version/codesecadoranalogpatch.ino
@@ -1,6 +1,6 @@
 // Definição dos pinos
 const int pinSensor = A0; // Pino do sensor TCRT 5000 (agora analógico)
-const int pinLedRed = 3; // Pino do LED vermelho (motor)
+const int MotorRele = 3; // Pino do Relé ( ativa o motor)
 const int pinLedGreen = 13; // Pino do LED verde (indicador de tempo)
 
 // Variáveis de controle
@@ -10,11 +10,11 @@ bool motorActive = false; // Indica se o motor está acionado
 void setup() {
 // Inicializa os pinos
 pinMode(pinSensor, INPUT); // Pino do sensor como entrada
-pinMode(pinLedRed, OUTPUT); // Pino do LED vermelho como saída
+pinMode(MotorRele, OUTPUT); // Pino do Relé como saída
 pinMode(pinLedGreen, OUTPUT); // Pino do LED verde como saída
 
 // Inicializa os LEDs como desligados
-digitalWrite(pinLedRed, LOW);
+digitalWrite(MotorRele, LOW);
 digitalWrite(pinLedGreen, LOW);
 
 Serial.begin(9600); // Inicia a comunicação serial (opcional, para depuração)
@@ -28,8 +28,8 @@ if (sensorValue < 900 && !motorActive) {
 Serial.println("Movimento Detectado");
 
 
-// Ativa o motor (LED vermelho)
-digitalWrite(pinLedRed, HIGH);
+// Ativa o motor (Ativa o Rele)
+digitalWrite(MotorRele, HIGH);
 motorActive = true;
 startTime = millis(); // Registra o tempo de início do motor
 
@@ -60,7 +60,7 @@ delay(500);
 // Se o tempo de 10 segundos foi atingido
 if (remainingTime <= 0) {
 // Desliga o motor (LED vermelho)
-digitalWrite(pinLedRed, LOW);
+digitalWrite(MotorRele, LOW);
 motorActive = false; // Desativa o motor
 digitalWrite(pinLedGreen, LOW); // Desliga o LED verde
 Serial.println("Tempo finalizado!");
@@ -69,7 +69,7 @@ Serial.println("Tempo finalizado!");
 // Verifica se a mão foi retirada (sensor para de detectar movimento)
 if (sensorValue >= 500) {
 // Desliga o motor imediatamente (ambos os LEDs desligados)
-digitalWrite(pinLedRed, LOW);
+digitalWrite(MotorRele, LOW);
 digitalWrite(pinLedGreen, LOW);
 motorActive = false; // Desativa o motor
 Serial.println("Mão retirada, secador desligado.");
@@ -82,7 +82,7 @@ sensorValue = analogRead(pinSensor);
 } else {
 // Caso não haja movimento, o motor está desligado e o LED verde está apagado
 digitalWrite(pinLedGreen, LOW);
-digitalWrite(pinLedRed, LOW);
+digitalWrite(MotorRele, LOW);
 }
 
 delay(3000); // Pequeno atraso para evitar leituras excessivas
